cdad39a6da5120f37e3e74e13a328df3296ae022
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-08-02 20:40:55 +0300

    add folder structure and react-app

---
+++ src/client/src/ts/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import "../style/style.scss";
+import style from "./app.module.scss";
+function App() {
+  return (
+    <div className={style.app}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
