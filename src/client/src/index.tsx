cb93530eb30d3f4eb7649632025ebf6e0d0bc1b3
rom-orlovich <rom-orlovich@cyber4s.dev>
2022-08-09 22:58:07 +0300

    add list compoent and check the fetch function

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
-import App from "./ts/App";
-import { store } from "./ts/state/store";
+import App from "./tsx/App";
+import { store } from "./tsx/state/store";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
