3918c62c3a593397f3db8d2b023e37cacc38f7fd
yuval <nhajeho1@gmail.com>
2022-08-09 15:16:24 +0300

    written first algorithm iteration

--- src/server/ga.py
+++ src/server/ga.py
@@ -1,55 +1,139 @@
-import random
-
+from copy import deepcopy
+from json import JSONEncoder
+from random import random, seed, randrange
 from deap import algorithms
 from deap import base
 from deap import creator
 from deap import tools
+from datetime import datetime, timedelta
+from typing import List
+from fake_data import fake_person, fake_task
 
-GOAL_STR = "Hey mate, cool GA!"
+from database_types import *
+
+
+START_TIME = datetime.today().replace(hour=10)
+END_TIME = datetime.today().replace(hour=18)
+
+
+class Time_table(TypedDict):
+    date: int
+    person: Person
+    task: Task
+
+
+individual_type = List[Time_table]
+
+tasks: List[Task] = [fake_task() for i in range(3)]
+
+people: List[Person] = [fake_person() for i in range(10)]
+
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
 toolbox = base.Toolbox()
 
-# Attribute generator
-toolbox.register("attr_char", lambda: chr(random.randint(31, 127)))
 
-# Structure initializers
-toolbox.register("individual", tools.initRepeat,
-                 creator.Individual, toolbox.attr_char, len(GOAL_STR))
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+def generate_random_table():
+    ind: individual_type = creator.Individual()
+    remaining_people = people[:]
+    curr_time = {task["id"]: START_TIME for task in tasks}
+    while True:
+        for task in tasks:
+            if not remaining_people:
+                return ind
+            rand_pip = remaining_people.pop(
+                randrange(len(remaining_people)))
+            ind.append(
+                {"date": curr_time[task["id"]], "person": rand_pip, "task": task})
+            curr_time[task["id"]] = curr_time[task["id"]] + \
+                timedelta(minutes=task["shift_duration"])
 
 
-def evalStrCompare(individual):
-    return sum([1 for i, c in enumerate(individual) if GOAL_STR[i] == c]),
+toolbox.register("population", tools.initRepeat, list, generate_random_table)
 
 
-def mutateStr(individual, indpb):
+def calc_required_roles_fullfield(individual: individual_type):
+    roles_nums = []
+    for task in tasks:
+        for role in task["required_people_per_shift"]:
+            role_num = role["num"]
+            for table_row in individual:
+                if table_row["task"]["id"] == task["id"]:
+                    if role["role"] == 'any' or \
+                            role["role"] in table_row["person"]["roles"]:
+                        role_num -= 1
+                        if role_num == 0:
+                            break
+            roles_nums.append(role_num)
+    return 1/(sum(roles_nums)+1)
+
+
+def calc_score_difference(individual: individual_type):
+    scores = []
+    for table_row in individual:
+        # calculate difference between scores, higer is better
+        scores.append(
+            abs(table_row["person"]["score"] - table_row["task"]["score"]))
+    return sum(scores)/len(scores)
+
+
+def evaluate(individual: individual_type):
+    return calc_required_roles_fullfield(individual)+calc_score_difference(individual),
+
+
+def mate(a: individual_type, b: individual_type, indpb: float = 0.1):
+    '''
+    There can be two types of exchanges between individuals.
+    Either you switch task, or you switch date
+    '''
+    ind_a = deepcopy(a)
+    ind_b = deepcopy(b)
+    for i, ind_a_row in enumerate(a):
+        if random() > indpb:
+            continue
+        pip_id = ind_a_row["person"]["id"]
+        for k, ind_b_row in enumerate(ind_b):
+            if ind_b_row["person"]["id"] == pip_id:
+                ind_a[i]["person"], ind_b[k]["person"] = ind_b[k]["person"], ind_a[i]["person"]
+    return ind_a, ind_b
+
+
+def mutate(individual: individual_type, indpb: float):
     for i in range(len(individual)):
-        if random.random() > indpb:
-            individual[i] = toolbox.attr_char()
+        if random() > indpb:
+            continue
+        k = randrange(len(individual))
+        individual[i]["person"], individual[k]["person"] = individual[k]["person"], individual[i]["person"]
     return individual,
 
 
-toolbox.register("evaluate", evalStrCompare)
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", mutateStr, indpb=0.05)
+toolbox.register("evaluate", evaluate)
+toolbox.register("mate", mate)
+toolbox.register("mutate", mutate, indpb=0.05)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
 
 def main():
-    random.seed(64)
+    seed(64)
 
-    pop = toolbox.population(n=500)
-    hof = tools.HallOfFame(5)
+    pop = toolbox.population(n=100)
+    hof = tools.HallOfFame(1)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("max", max)
 
     pop, log = algorithms.eaSimple(
-        pop, toolbox, cxpb=0.5, mutpb=0.3, ngen=50, halloffame=hof)
+        pop, toolbox, cxpb=0.5, mutpb=0.3, ngen=100, halloffame=hof, stats=stats)
 
-    return pop  # , log, hof
+    return pop
 
 
 if __name__ == "__main__":
-    pop = main()
-    print(''.join(pop[-1]))
+    pop: List[individual_type] = main()
+    best = pop[-1]
+    for i, item in enumerate(best):
+        best[i] = {"task": item["task"],
+                   "person": item["person"], "date": item["date"].timestamp()}
+    best.sort(key=lambda x: x["date"])
+    print(JSONEncoder().encode(best))
