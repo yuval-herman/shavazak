c0860f81bafd9a509a10aca502ed6148ed75e5e7
yuval <nhajeho1@gmail.com>
2022-08-30 18:04:28 +0300

    added simple table view
    Co-authored-by: Alex Arden <alexarden@users.noreply.github.com>

---
+++ client/src/App/App.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { Shift, Task } from "../types";
+import style from "./App.module.scss";
+
+interface Props {
+	tasks: Task[];
+}
+
+function ShiftDiv(props: { shift: Shift }) {
+	return (
+		<div className={style.shift}>
+			{props.shift.people.map((person) => (
+				<div key={person.id}>{person.name}</div>
+			))}
+		</div>
+	);
+}
+
+function TaskDiv(props: { task: Task; width: number }) {
+	return (
+		<div
+			key={props.task.id}
+			className={style.shifts}
+			style={{ width: props.width }}
+		>
+			{props.task.shifts.map((shift) => (
+				<ShiftDiv shift={shift} />
+			))}
+		</div>
+	);
+}
+
+function TasksTable(props: Props) {
+	const tasks = props.tasks;
+	return (
+		<div>
+			<header className={style.headers}>
+				{tasks.map((task) => (
+					<div key={task.id}>{task.name}</div>
+				))}
+			</header>
+			<main className={style.tasks}>
+				{tasks.map((task) => (
+					<TaskDiv task={task} width={100 / tasks.length} />
+				))}
+			</main>
+		</div>
+	);
+}
+
+function App() {
+	const [tasks, setTasks] = useState<Task[]>();
+	useEffect(() => {
+		fetch("/randomtable").then((res) => res.json().then(setTasks));
+	}, []);
+
+	return (
+		<div className={style.main}>
+			{tasks ? <TasksTable tasks={tasks} /> : "Loading tasks"}
+		</div>
+	);
+}
+export default App;
---
+++ client/src/types.d.ts
@@ -0,0 +1,26 @@
+export interface RequiredPeoplePerShift {
+	amount: number;
+	role: string;
+}
+
+export interface Person {
+	id: number;
+	name: string;
+	roles: string[];
+	score: number;
+	status: number;
+}
+
+export interface Shift {
+	date: number;
+	people: Person[];
+}
+
+export interface Task {
+	id: number;
+	name: string;
+	required_people_per_shift: RequiredPeoplePerShift[];
+	score: number;
+	shift_duration: number;
+	shifts: Shift[];
+}
