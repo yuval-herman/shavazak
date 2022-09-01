df8410e5e7268bd2bf17437f49f15171fbd799d8
yuval <nhajeho1@gmail.com>
2022-09-01 21:02:03 +0300

    update saveTask to match savePerson

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,15 +1,18 @@
 import { isPerson, Person, Task } from "./types";
 
 export function savePerson(person: Person) {
-  const people: Person[] = JSON.parse(localStorage.getItem("people") ?? "[]");
-  const index = people.findIndex((pip) => pip.id === person.id);
-  if (index === -1) people.push(person);
-  else people[index] = person;
-  localStorage.setItem("people", JSON.stringify(people));
+	const people: Person[] = JSON.parse(localStorage.getItem("people") ?? "[]");
+	const index = people.findIndex((pip) => pip.id === person.id);
+	if (index === -1) people.push(person);
+	else people[index] = person;
+	localStorage.setItem("people", JSON.stringify(people));
 }
 
 export function saveTask(task: Task) {
-  const tasks: Task[] = JSON.parse(localStorage.getItem("tasks") ?? "[]");
-  tasks.push(task);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+	const tasks: Task[] = JSON.parse(localStorage.getItem("tasks") ?? "[]");
+	const index = tasks.findIndex((t) => t.id === task.id);
+	if (index === -1) tasks.push(task);
+	else tasks[index] = task;
+	tasks.push(task);
+	localStorage.setItem("tasks", JSON.stringify(tasks));
 }
