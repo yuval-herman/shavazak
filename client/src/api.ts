36ada7dab1c6892fe855079dc45aba1b2fdc90db
yuval <nhajeho1@gmail.com>
2022-09-02 11:45:11 +0300

    added getter functions for people and tasks

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,7 @@
 import { isPerson, Person, Task } from "./types";
 
 export function savePerson(person: Person) {
-	const people: Person[] = JSON.parse(localStorage.getItem("people") ?? "[]");
+	const people = getPeople();
 	const index = people.findIndex((pip) => pip.id === person.id);
 	if (index === -1) people.push(person);
 	else people[index] = person;
@@ -9,10 +9,18 @@ export function savePerson(person: Person) {
 }
 
 export function saveTask(task: Task) {
-	const tasks: Task[] = JSON.parse(localStorage.getItem("tasks") ?? "[]");
+	const tasks = getTasks();
 	const index = tasks.findIndex((t) => t.id === task.id);
 	if (index === -1) tasks.push(task);
 	else tasks[index] = task;
 	tasks.push(task);
 	localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+export function getTasks(): Task[] {
+	return JSON.parse(localStorage.getItem("tasks") ?? "[]");
+}
+
+export function getPeople(): Person[] {
+	return JSON.parse(localStorage.getItem("people") ?? "[]");
 }
