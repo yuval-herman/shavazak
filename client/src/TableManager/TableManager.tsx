800f5669d4824867aac3becf58ac6f5d637dddce
yuval <nhajeho1@gmail.com>
2022-09-04 17:24:30 +0300

    added mainNavbar to tableManager

--- client/src/TableManager/TableManager.tsx
+++ client/src/TableManager/TableManager.tsx
@@ -1,30 +1,34 @@
 import { Link, Outlet } from "react-router-dom";
+import { MainNavbar } from "../MainNavbar/MainNavbar";
 import style from "./TableManager.module.scss";
 
 function TableManager() {
 	return (
-		<div className={style.main}>
-			<nav className={style.sidebar}>
-				<span>
-					<Link to={"/"}>home</Link>
-				</span>
-				<span>
-					<Link to={"addperson"}>add person</Link>
-				</span>
-				<span>
-					<Link to={"addtask"}>add task</Link>
-				</span>
-				<span>
-					<Link to={"viewtasks"}>show tasks</Link>
-				</span>
-				<span>
-					<Link to={"viewpeople"}>show people</Link>
-				</span>
-			</nav>
-			<div>
-				<Outlet />
+		<>
+			<MainNavbar />
+			<div className={style.main}>
+				<nav className={style.sidebar}>
+					<span>
+						<Link to={"/"}>home</Link>
+					</span>
+					<span>
+						<Link to={"addperson"}>add person</Link>
+					</span>
+					<span>
+						<Link to={"addtask"}>add task</Link>
+					</span>
+					<span>
+						<Link to={"viewtasks"}>show tasks</Link>
+					</span>
+					<span>
+						<Link to={"viewpeople"}>show people</Link>
+					</span>
+				</nav>
+				<div>
+					<Outlet />
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
