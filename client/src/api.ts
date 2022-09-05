c8287abf0ae78cd45c0ad5b9108ed953d4f00d52
yuval <nhajeho1@gmail.com>
2022-09-05 12:25:56 +0300

    added reload to api calls that change data

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,15 +1,19 @@
 import { isPerson, Person, Task } from "./types";
 
-export async function fetchPOST(input: RequestInfo | URL, data: any, init?: RequestInit) {
-  return (
-    await fetch(input, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: typeof data === "object" ? JSON.stringify(data) : data,
-    })
-  ).json();
+export async function fetchPOST(
+	input: RequestInfo | URL,
+	data: any,
+	init?: RequestInit
+) {
+	return (
+		await fetch(input, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: typeof data === "object" ? JSON.stringify(data) : data,
+		})
+	).json();
 }
 
 export async function fetchJSON(input: RequestInfo | URL, init?: RequestInit) {
@@ -17,39 +21,43 @@ export async function fetchJSON(input: RequestInfo | URL, init?: RequestInit) {
 }
 
 export function savePerson(person: Person) {
-  const people = getPeople();
-  const index = people.findIndex((pip) => pip.id === person.id);
-  if (index === -1) people.push(person);
-  else people[index] = person;
-  localStorage.setItem("people", JSON.stringify(people));
+	const people = getPeople();
+	const index = people.findIndex((pip) => pip.id === person.id);
+	if (index === -1) people.push(person);
+	else people[index] = person;
+	localStorage.setItem("people", JSON.stringify(people));
+	window.location.reload();
 }
 
 export function saveTask(task: Task) {
-  const tasks = getTasks();
-  const index = tasks.findIndex((t) => t.id === task.id);
-  if (index === -1) tasks.push(task);
-  else tasks[index] = task;
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+	const tasks = getTasks();
+	const index = tasks.findIndex((t) => t.id === task.id);
+	if (index === -1) tasks.push(task);
+	else tasks[index] = task;
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+	window.location.reload();
 }
 
 export function deletePerson(id: string) {
-  const people = getPeople();
-  const index = people.findIndex((pip) => pip.id === id);
-  people.splice(index, 1);
-  localStorage.setItem("people", JSON.stringify(people));
+	const people = getPeople();
+	const index = people.findIndex((pip) => pip.id === id);
+	people.splice(index, 1);
+	localStorage.setItem("people", JSON.stringify(people));
+	window.location.reload();
 }
 
 export function deleteTask(id: string) {
-  const tasks = getTasks();
-  const index = tasks.findIndex((t) => t.id === id);
-  tasks.splice(index, 1);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+	const tasks = getTasks();
+	const index = tasks.findIndex((t) => t.id === id);
+	tasks.splice(index, 1);
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+	window.location.reload();
 }
 
 export function getTasks(): Task[] {
-  return JSON.parse(localStorage.getItem("tasks") ?? "[]");
+	return JSON.parse(localStorage.getItem("tasks") ?? "[]");
 }
 
 export function getPeople(): Person[] {
-  return JSON.parse(localStorage.getItem("people") ?? "[]");
+	return JSON.parse(localStorage.getItem("people") ?? "[]");
 }
