045d28a686da297c657fa820ec48dfd7564f727d
yuval <nhajeho1@gmail.com>
2022-09-07 17:09:08 +0300

    updated build script and added deploy

--- build.py
+++ build.py
@@ -8,6 +8,7 @@ from subprocess import run
 class Actions(Enum):
     build = auto()
     serve = auto()
+    deploy = auto()
 
 
 parser = argparse.ArgumentParser(description='Build project')
@@ -31,6 +32,14 @@ def build():
     copyDir('server', 'deploy/server')
 
 
+def deploy():
+    run(['scp', '-r', 'deploy/',  'root@172.104.236.178:/root'])
+    run(['ssh', 'root@172.104.236.178',
+        'cp', '-r', '/root/deploy/*', '/root'])
+    run(['ssh', 'root@172.104.236.178',
+        'systemctl', 'restart', 'shavzak-api.service'])
+
+
 def serve():
     run(['flask', '--debug', 'run'], cwd='server')
 
@@ -38,6 +47,10 @@ def serve():
 if args.action == Actions.build.name:
     print('building')
     build()
+    print('run again with the deploy option in order to send the changes to the server')
 elif args.action == Actions.serve.name:
     print('serving')
     serve()
+elif args.action == Actions.deploy.name:
+    print("deploying")
+    deploy()
