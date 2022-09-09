1a1147edcc0889f2afbf2666610cb6cc96b188ac
yuval <nhajeho1@gmail.com>
2022-09-09 15:57:22 +0300

    added better controls for setting start and end time

--- client/src/components/TasksView/TasksView.tsx
+++ client/src/components/TasksView/TasksView.tsx
@@ -1,55 +1,48 @@
-import { ChangeEvent, SyntheticEvent, useContext, useState } from "react";
+import {
+	ChangeEvent,
+	MouseEvent,
+	SyntheticEvent,
+	useContext,
+	useRef,
+	useState,
+} from "react";
 import { TasksContext } from "../../context/TasksContext";
 import { DataTable } from "../DataTable/DataTable";
 import style from "./TasksView.module.scss";
 
 function TasksView() {
 	const tasks = useContext(TasksContext);
+	const start_time = useRef<HTMLInputElement>(null);
+	const end_time = useRef<HTMLInputElement>(null);
 
-	function handleChange(event: ChangeEvent<HTMLInputElement>) {
-		const inputName = event.target.getAttribute("name")!;
-		console.log(event.target.value);
-
-		const value = event.target.value.length
-			? (() => {
-					const time = new Date();
-					const [hours, minutes, _] = event.target.value.split(":");
-					time.setHours(parseInt(hours));
-					time.setMinutes(parseInt(minutes));
-					return time;
-			  })()
-			: new Date();
-		tasks[inputName === "start_time" ? "setStartTime" : "setEndTime"](value);
-		console.log(tasks);
+	function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+		function getDate(str: string) {
+			const time = new Date();
+			const [hours, minutes, _] = str.split(":");
+			time.setHours(parseInt(hours));
+			time.setMinutes(parseInt(minutes));
+			return time;
+		}
+		if (!start_time.current || !end_time.current) return;
+		tasks.setStartTime(getDate(start_time.current.value));
+		tasks.setEndTime(getDate(end_time.current.value));
 	}
-
-	const formatter = Intl.DateTimeFormat("default", {
-		hour12: false,
-		hour: "numeric",
-		minute: "numeric",
-	});
 
 	return (
 		<div>
 			<div className={style.times}>
 				<label>
 					Tasks Start Time:{" "}
-					<input
-						onChange={handleChange}
-						value={formatter.format(tasks.start_time)}
-						type="time"
-						name="start_time"
-					/>
+					<input ref={start_time} type="time" name="start_time" />
 				</label>
 				<label>
-					Tasks End Time:{" "}
-					<input
-						onChange={handleChange}
-						value={formatter.format(tasks.end_time)}
-						type="time"
-						name="end_time"
-					/>
+					Tasks End Time: <input ref={end_time} type="time" name="end_time" />
 				</label>
+				<button onClick={handleClick}>Save</button>
+			</div>
+			<div>
+				<p>Start: {tasks.start_time.toTimeString()}</p>
+				<p>end: {tasks.end_time.toTimeString()}</p>
 			</div>
 			<DataTable
 				data={tasks.tasks}
