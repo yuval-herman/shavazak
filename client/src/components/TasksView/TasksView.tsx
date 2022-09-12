e8fbbdb75892b046163cb7ff9ffdde89460919c8
yuval <nhajeho1@gmail.com>
2022-09-12 11:57:55 +0300

    replaced data table with details list

--- client/src/components/TasksView/TasksView.tsx
+++ client/src/components/TasksView/TasksView.tsx
@@ -8,13 +8,18 @@ import {
 } from "react";
 import { TasksContext } from "../../context/TasksContext";
 import { DataTable } from "../DataTable/DataTable";
+import DetailsList from "../DetailsList/DetailesList";
 
 function TasksView() {
 	const tasks = useContext(TasksContext);
 
 	return (
-		<DataTable
-			data={tasks.tasks}
+		<DetailsList
+			data={tasks.tasks.map((task) => {
+				const { name, shifts, ...newObj } = task;
+				return newObj;
+			})}
+			names={tasks.tasks.map((task) => task.name)}
 			delete={tasks.delete}
 			edit={(id: string) => "addtask?id=" + id}
 		/>
