6e6283b96c61d30f3efe58ffa0e1d436219a6136
yuval <nhajeho1@gmail.com>
2022-09-13 14:20:34 +0300

    added active link indication for MainNavbar

--- client/src/components/MainNavbar/MainNavbar.tsx
+++ client/src/components/MainNavbar/MainNavbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import style from "./MainNavbar.module.scss";
 import helmet from "../../media/icons/helmet.svg";
 
@@ -6,9 +6,24 @@ export function MainNavbar() {
 	return (
 		<nav className={style.navbar}>
 			<img src={helmet} />
-			<Link to="/">table view</Link>
-			<Link to="/tablemanager">manager view</Link>
-			<Link to="/randomdata">get random data</Link>
+			<NavLink
+				className={({ isActive }) => (isActive ? style.activeLink : "")}
+				to="/"
+			>
+				table view
+			</NavLink>
+			<NavLink
+				className={({ isActive }) => (isActive ? style.activeLink : "")}
+				to="/tablemanager"
+			>
+				manager view
+			</NavLink>
+			<NavLink
+				className={({ isActive }) => (isActive ? style.activeLink : "")}
+				to="/randomdata"
+			>
+				get random data
+			</NavLink>
 			<p>Shavzak</p>
 		</nav>
 	);
